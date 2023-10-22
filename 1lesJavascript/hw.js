428db79994fc3eb7c58a5be5129753386bd914c3
Srzhstr <138973252+Srzhstr@users.noreply.github.com>
2023-10-22 12:28:05 +0300

    Add files via upload

---
+++ 1lesJavascript/hw.js
@@ -0,0 +1,43 @@
+let firstValueNickName = 'Nick';
+let secondValueNickName ='Name';
+let result = firstValueNickName+secondValueNickName;
+console.log(result);
+
+
+let userScoreValue = '15';
+let userValueScore = '30';
+let resultValueScore = userScoreValue + userValueScore;
+console.log(resultValueScore);
+
+
+let a = 55;
+let b = 60;
+let c = 34;
+let numberResult = a+b-c;
+console.log(numberResult);
+
+let QuantityOfProducts = Number(prompt('Type your quantity of products in '));
+console.log(QuantityOfProducts);
+//500 штук
+
+let ProductPrice = Number(prompt('Type your product price'));
+console.log(ProductPrice);
+//16 UAH
+
+let multiply = QuantityOfProducts*ProductPrice;
+console.log(multiply);
+
+alert('The total amount is 8000UAH, but you have a 5% discount, do you want to use it?');
+alert('your total amount with discount = 7600UAH. thank you for buying');
+//----------------------------------------------------------
+
+let minusValue = 500 - 16;
+console.log(minusValue);
+
+let summ = 16 + 500;
+console.log(summ);
+
+let devide = 500/16;
+console.log(devide);
+
+
