27a8a8a6210e8ee6cabf2c6ab2ee99a6d7b0d89e
Srzhstr <138973252+Srzhstr@users.noreply.github.com>
2023-10-25 15:42:40 +0300

    Add files via upload

---
+++ 02 JS HW/hw.js
@@ -0,0 +1,36 @@
+let UserAge = Number(prompt('Type your age')); 
+ 
+ 
+let GeneralAge = 18; 
+let maxValue = 0; 
+ 
+maxValue = GeneralAge>UserAge ? GeneralAge : UserAge; 
+ 
+console.log(`${maxValue==UserAge}`); 
+ 
+//2nd Task 
+ 
+let firstValue = Number(prompt(`Enter your first math`)); 
+let secondValue = Number(prompt(`Enter your second math`));
+MathOperators = prompt('Enter your math operation');
+result = 0;
+if (MathOperators == '+') {
+    result = firstValue + secondValue;
+    console.log(`${firstValue}${MathOperators}${secondValue}=${result}`);
+  }
+  if (MathOperators == '-') {
+    result = firstValue - secondValue;
+    console.log(`${firstValue}${MathOperators}${secondValue}=${result}`);
+  }
+  if (MathOperators == '/') {
+    if (secondValue == 0) {
+      console.log('Cannot divide by 0');
+    } else {
+      result = firstValue / secondValue;
+      console.log(`Result = ${result}`);
+    }
+  }
+  if (MathOperators == '*') {
+    result = firstValue * secondValue;
+    console.log(`Result  = ${result}`);
+  }
